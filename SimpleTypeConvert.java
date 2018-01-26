e662157624eb519d81412784490c9b784313cfa6
夏飞 <574401281@qq.com>
2018-01-26 22:42:34 +0800

    Update SimpleTypeConvert.java

--- SimpleTypeConvert.java
+++ SimpleTypeConvert.java
@@ -9,7 +9,15 @@ public class SimpleTypeConvert {
             } catch (ParseException e) {
                 throw new IllegalArgumentException("convert date error" + e.getMessage());
             }
-        } else if (Number.class.isAssignableFrom(targetClass)) {
+        }else if(java.sql.Date.class.isAssignableFrom(targetClass)){
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date date= simpleDateFormat.parse(textAsValue);
+                return (T) new java.sql.Date(date.getTime());
+            } catch (ParseException e) {
+                throw new IllegalArgumentException("convert date error" + e.getMessage());
+            }
+        }else if (Number.class.isAssignableFrom(targetClass)) {
             return (T) NumberUtils.parseNumber(textAsValue, (Class) targetClass);
         } else {
             throw new IllegalArgumentException("Cannot convert String [" + textAsValue + "] to target class [" + targetClass.getName() + "]");
