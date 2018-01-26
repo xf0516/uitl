015faf2ba5f0389b88cb774836de8e3ad24f841a
夏飞 <574401281@qq.com>
2018-01-26 22:39:13 +0800

    Create SimpleTypeConvert.java

---
+++ SimpleTypeConvert.java
@@ -0,0 +1,18 @@
+public class SimpleTypeConvert {
+    public static <T> T convert(String textAsValue, Class<T> targetClass) {
+        if (String.class.isAssignableFrom(targetClass)) {
+            return (T) textAsValue;
+        } else if (Date.class.isAssignableFrom(targetClass)) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                return (T) simpleDateFormat.parse(textAsValue);
+            } catch (ParseException e) {
+                throw new IllegalArgumentException("convert date error" + e.getMessage());
+            }
+        } else if (Number.class.isAssignableFrom(targetClass)) {
+            return (T) NumberUtils.parseNumber(textAsValue, (Class) targetClass);
+        } else {
+            throw new IllegalArgumentException("Cannot convert String [" + textAsValue + "] to target class [" + targetClass.getName() + "]");
+        }
+    }
+}
